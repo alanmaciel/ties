d8c907bd1850dd37f18eabd093d5362a105fb023
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-11-17 17:35:02 -0600

    Create Tie model and added starts and ends datetime fields
    Create Tie model

---
+++ db/migrate/20191117193509_create_ties.rb
@@ -0,0 +1,10 @@
+class CreateTies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ties do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
